b530897262ac43522852eb383c1872ca4db6b8f0
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-05-22 11:10:56 -0500

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,17 @@
+<?php
+ $host = 'localhost';
+$user = 'joe773_joe773';
+$password = '12345';
+$dbname = 'joe773_loginSocialMedia';
+
+$dsn = 'mysql:host=' . $host . '; dbname=' . $dbname;
+
+try {
+$pdo = new PDO($dsn, $user, $password);
+$pdo->setattribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// echo "Connected successfully";
+} catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
