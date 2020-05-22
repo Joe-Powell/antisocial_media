cba171ee4424e79f9fac864ee07249dbc4d0bd6b
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-05-22 11:15:37 -0500

    Create navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,19 @@
+<?php 
+    echo '<link rel="stylesheet" href="../style.css">'
+?>
+
+
+<nav class="navbar">
+    <ul>
+        <a href="index.php"><li>Home</li></a>
+        <a href="posts.php"><li>Posts</li></a>
+        <a href="post.php"><li>Post</li></a>
+        
+       <?php  if(isset($_SESSION['userId'])){ ?>
+        <li><form class="logout" action="includes/logout.php" method="post">
+        <button type="submit" name="logout-submit">Logout</button>
+            </form></li>
+
+    <?php } ?>        
+    </ul>
+</nav>
