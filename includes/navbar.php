021bb96bfd0013956467d3582c7e23fadaa501d0
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-05-24 18:46:00 -0500

    Update navbar.php
    added some emoji's  always remove U+ and put &#x then have : semicolon at the end

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,19 +1,22 @@
+
 <?php 
     echo '<link rel="stylesheet" href="../style.css">'
 ?>
-
 <div class="containAll">
-    <nav class="navbar">
-        <ul>
-            <a href="index.php"><li>Home</li></a>
-            <a href="posts.php"><li>Posts</li></a>
-            <a href="post.php"><li>Post</li></a>
 
-           <?php  if(isset($_SESSION['userId'])){ ?>
-            <li><form class="logout" action="includes/logout.php" method="post">
-            <button type="submit" name="logout-submit">Logout</button>
-                </form></li>
+<nav class="navbar">
+    <ul>
+        <a href="index.php"><li><ion-icon name="home-outline"></ion-icon> Home</li></a>
+        <a href="posts.php"><li> &#x1F4C4; Newsfeed</li></a>
+        <a href="post.php"><li> &#x1F4E5; Post</li></a>
 
-        <?php } ?>        
-        </ul>
-    </nav>
+    <?php  if(isset($_SESSION['userId'])){ ?>
+        
+        <li><form class="logout" action="includes/logout.php" method="post">
+        <button type="submit" name="logout-submit">&#x274C; Logout</button>
+            </form></li>
+
+    <?php } ?>        
+
+    </ul>
+</nav>
