b3c0bd8a6307668db14fced99bd68bcf008ed986
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-05-24 18:31:47 -0500

    Update navbar.php
    wrap everything in a div so it can be display flex to make use of the flex-direction property.

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,18 +2,18 @@
     echo '<link rel="stylesheet" href="../style.css">'
 ?>
 
+<div class="containAll">
+    <nav class="navbar">
+        <ul>
+            <a href="index.php"><li>Home</li></a>
+            <a href="posts.php"><li>Posts</li></a>
+            <a href="post.php"><li>Post</li></a>
 
-<nav class="navbar">
-    <ul>
-        <a href="index.php"><li>Home</li></a>
-        <a href="posts.php"><li>Posts</li></a>
-        <a href="post.php"><li>Post</li></a>
-        
-       <?php  if(isset($_SESSION['userId'])){ ?>
-        <li><form class="logout" action="includes/logout.php" method="post">
-        <button type="submit" name="logout-submit">Logout</button>
-            </form></li>
+           <?php  if(isset($_SESSION['userId'])){ ?>
+            <li><form class="logout" action="includes/logout.php" method="post">
+            <button type="submit" name="logout-submit">Logout</button>
+                </form></li>
 
-    <?php } ?>        
-    </ul>
-</nav>
+        <?php } ?>        
+        </ul>
+    </nav>
