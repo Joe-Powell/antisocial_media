ffb48b62572eafc15fac387195fbebc7639900dd
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-05-26 00:13:28 -0500

    Update addpostform.php

--- addpostform.php
+++ addpostform.php
@@ -1,27 +1,16 @@
 
-    <form class="add-post" method="POST" action="post.php" >
+     <form class="add-post" method="POST" action="post.php" >
+         <input type="hidden" name="author" value="<?php echo  $_SESSION['username'] ?>">
        
-        <div >
-            <input type="hidden" name="author" value="<?php echo  $_SESSION['username'] ?>">
-        </div>
 
         <div class='textAreaDiv'>
-            <h2>Add Post</h2>
-            <textarea name="body" type='text' > </textarea>
-        </div>
-
-        <div >
-            <input type="hidden" name="inputId" value="<?php echo $_SESSION['userId']; ?>">
-        </div>
-
+            <h2>Add a new post here</h2>
+            <textarea name="body" type='text' >
+            </textarea>
             <input type="submit" name="submit" value="Submit" > 
 
-        <form action='upload.php' method='post' enctype='multipart/form-data'> 
-            <input type='file' name='file'>  
-            <button type='submit' name='uploadFile' >Upload</button> 
-            
-        </form>
+        </div>
 
+        <input type="hidden" name="inputId" value="<?php echo $_SESSION['userId']; ?>">
 
-
-</form >
+    </form>
