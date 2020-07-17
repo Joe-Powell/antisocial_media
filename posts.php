a9990d0853ce8381f2ca300950940b0e18ade3f6
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-07-17 14:14:45 -0500

    Update posts.php

--- posts.php
+++ posts.php
@@ -73,13 +73,7 @@ if(isset($_SESSION['userId'])) {
         <source src='uploadVideos/".$post->video."' type='video/mp4'>
       </video>"; 
     } 
-      elseif($post->video) {
-        echo   "<P>$post->body</p>
-        <video width='300' height='210' controls>
-        <source src='uploadVideos/".$post->video."' type='video/mp4'>
-        </video>";  
-      
-    }
+    
       elseif($post->image) {
         echo   "<P>$post->body</p>
               <img src='uploads/".$post->image."' height='100' width='100'> 
