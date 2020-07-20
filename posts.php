492f262d8ada5d063d01fe5a3fb4284fc5ca7aad
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-07-20 09:10:25 -0500

    Update posts.php

--- posts.php
+++ posts.php
@@ -8,24 +8,17 @@
 
 if(isset($_POST['delete'])) {
     require "./config/db.php";
-
     $the_id = $_POST['the_id'];
-  
-   $stmt = $pdo->prepare("DELETE FROM posts Where id = ? ");  
-      
+    $stmt = $pdo->prepare("DELETE FROM posts Where id = ? ");  
     $stmt->execute([$the_id]);
     
-    
-     
     // by putting the delete above the stmt execute I am able to get the delete the first time otherwise have to refresh twice for results
     // or even have this code below it for the first refresh to work or submission.. not sureif this is only with PDO or not...
     // $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC ");
     //$stmt->execute();
     //$posts = $stmt->fetchAll();
   
-    
-    
-  }
+    }
   
   
   // see this here below also had to be above the if(isset($_SESSION['userId'] and works first submission. 
@@ -33,34 +26,25 @@ if(isset($_POST['delete'])) {
      
   
   if(isset($_POST['submit'])) {
-     require "./config/db.php";
-
+    require "./config/db.php";
     $body = $_POST['the_body'];
     $the_id = $_POST['the_id'];
-
-   $stmt = $pdo->prepare("UPDATE posts SET body='$body' WHERE id = ? ");   
-   $stmt->execute([$the_id]);
+    $stmt = $pdo->prepare("UPDATE posts SET body='$body' WHERE id = ? ");   
+    $stmt->execute([$the_id]);
 }
 
 
 
 if(isset($_SESSION['userId'])) {
     require "./config/db.php";
-  
-     $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC ");
+    $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC ");
     $stmt->execute();
     $posts = $stmt->fetchAll();
     
-    
-    
-
 }else {
-   
-    
-    // for offline users
-
+   // for offline users
     require "./config/db.php";
-      $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC ");
+    $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC ");
     $stmt->execute();
     $posts = $stmt->fetchAll();
    
@@ -68,64 +52,80 @@ if(isset($_SESSION['userId'])) {
 }
 
 
-
-
-
-
-
-
-
-
 ?>
+
+
 
 <div class="postsContainer">
 <h2 class='heading'>Posts Below</h2>
 
 <?php foreach($posts as $post) { ?>
 
-
-
-
-
 <div class='containerDivs'>
-    <h2><?php echo $post->author ?></h2>
-    <h4><?php echo $post->body ?></h4>
+    <h2><?php echo $post->author ?></h2><br>
+    
 
-    <?php if($post->image) {
-      echo   "<img src='uploads/".$post->image."' height='100' width='100'>";  
+
+    <?php    
+    if($post->video && $post->image !== '.') {
+      echo   "<P>$post->body</p>
+      <img src='uploads/".$post->image."' width='300' height='210'>
+
+      <video width='300' height='210' controls>
+      <source src='uploadVideos/".$post->video."' type='video/mp4'>
+      </video>
+      ";  
+    
+  }
+    elseif($post->video ) {
+        echo   "<P>$post->body</p>
+        <video width='300' height='210' controls>
+        <source src='uploadVideos/".$post->video."' type='video/mp4'>
+        </video>";  
+      
     }
-    ?>
-<?php
-    if($_SESSION['userId'] == $post->inputId) { ?>
+    elseif($post->image !== '.') {
+      echo   "<P>$post->body</p>
+      <img src='uploads/".$post->image."' height='100' width='100'>
+      
+      ";  
+    
+  }else{
+            echo   "<P>$post->body</p>";
 
+  }
+    
+     
+
+
+   
+    ?>
+
+ 
+
+
+
+    <?php if($_SESSION['userId'] == $post->inputId) { ?>
         <div class="btnsWrapper">
             <button type='submit' class='editBtn'>Edit</button>
-
-           </div>
-           
-           
-
-       <div class="editAbsoluteDiv">
-
-            <form class='formToEdit' action="posts.php" method="POST">
-                <ion-icon name="close-outline"></ion-icon>
-                <textarea name='the_body' type="text" value="<?php echo $post->body  ?>" ><?php echo $post->body  ?></textarea>
-                <!-- <input type='text' value="<?php // echo $post->image ?>" > -->
-
-                <input name='the_id' type="hidden" value="<?php echo $post->id  ?>">
-
-                <input name='submit' type="submit" value="Save Changes">
-                 <button  name='delete' type='submit' class='deleteBtn'>Delete</button>
-           
-                
-          </form>
-          
-           
         </div>
-      
-       
-      
-    
+            
+            
+
+    <div class="editAbsoluteDiv">
+        <form class='formToEdit' action="posts.php" method="POST">
+            <ion-icon name="close-outline"></ion-icon>
+            <textarea name='the_body' type="text" value="<?php echo $post->body  ?>" ><?php echo $post->body  ?></textarea>
+            <!-- <input type='text' value="<?php // echo $post->image ?>" > -->
+            <input name='the_id' type="hidden" value="<?php echo $post->id  ?>">
+            <input name='submit' type="submit" value="Save Changes">
+            <button  name='delete' type='submit' class='deleteBtn'>Delete</button>
+        </form>
+    </div>
+        
+        
+        
+        
     <?php } ?>    
   
 </div>
