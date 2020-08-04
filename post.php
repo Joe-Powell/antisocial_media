a415fd720201029701be594f17f68714ed4426ca
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-08-04 09:29:28 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,12 +1,17 @@
-<?php session_start(); ?>
+<?php session_start(); 
+require "./config/db.php";
+require "./includes/header.php";
 
-<?php require "./includes/header.php";?>
+
+
+?>
 
 <?php
 
 
 if(isset($_POST['submit'])) {
     require "./config/db.php";
+
     if(isset($_FILES["file"])) {
         $file = $_FILES["file"];
         //print_r($file);
@@ -14,31 +19,22 @@ if(isset($_POST['submit'])) {
         $fileType = $file['type'];
         $fileTmpName = $file['tmp_name'];
         $fileError= $file['error'];
-        $fileSize = $file['size'];
-        $fileExt = explode('.', $fileName);
-        $fileActualExt = strtolower(end($fileExt));
-        //print_r($fileTmpName);
-        $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+      
+        print_r($fileTmpName);
+      
 
-        $explode_filename = explode('.', $fileName);
-        $newFileName = $explode_filename[0];
+        $fileDestination = 'uploads/'.$fileName;
 
-        $fileNameNew = $newFileName . "." . $fileActualExt;   //  the file and extension, I already made image column in database to catch it
+      
 
-        $fileDestination = 'uploads/'.$fileNameNew;
-
-        if (in_array($fileActualExt, $allowed)) {
-            if ($fileSize < 1000000) {
-                move_uploaded_file($fileTmpName, $fileDestination);
-                }
-        }
+        move_uploaded_file($fileTmpName, $fileDestination);
 
         
 
         $body = $_POST['body'];
         $author = $_POST['author'];
         $inputId = $_POST['inputId'];
-        $fileNameNew = $newFileName . "." . $fileActualExt;  // just declairing again to show whats happening
+        
 
 
 
@@ -46,17 +42,12 @@ if(isset($_POST['submit'])) {
 
 
         if(isset($_FILES["videoFile"])) {
-            require "./config/db.php";
-    
             $files = $_FILES["videoFile"];
             $fileNames = $files['name'];
             $fileErrors = $files['error'];
-            //   print_r($fileName);
             $fileTypes = $files['type'];
             $fileTmpNames = $files['tmp_name'];
-            //print_r($files);
-            //print_r($fileTmpNames); // not getting right now
-            //print_r($fileErrors); // not getting right now
+          
             $fileSizes = $files['size'];
                 
             
@@ -67,12 +58,12 @@ if(isset($_POST['submit'])) {
         }   
     
 
-        $stmt = $pdo->prepare("INSERT INTO posts (author, body, inputId, image, video ) Values(?, ?, ?, ?, ?)");
-        $stmt->execute([ $author, $body, $inputId, $fileNameNew, $fileNames ]);
-        
-       
+      
     }
 
+    $stmt = $pdo->prepare("INSERT INTO posts (author, body, inputId, image, video ) Values(?, ?, ?, ?, ?)");
+    $stmt->execute([ $author, $body, $inputId, $fileName, $fileNames ]);
+    
 
 
 
@@ -87,7 +78,6 @@ if(isset($_SESSION['userId'])) {
     
     
 }else{
-    //header('Location: http://myblogs.live/loginSocialMediaPosts/index.php');
     
     echo 'session not started here';
 }
