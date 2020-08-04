c4ac83343d6e75a49fb8bae5508f89cb4931e4ed
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-08-04 09:31:00 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,59 @@
 <?php session_start();
+        require "./config/db.php";
+
+
+        ///// upload new profile picture
+
+        if(isset($_POST['submit_new_pro_pic'])){
+            require "./config/db.php";
+
+            $sessionid =  $_SESSION['userId'];
+
+            $file = $_FILES['profileUpload'];
+          
+            //print_r($file);
+            $fileName = $file['name'];
+            $fileType = $file['type'];
+            $fileTmpName = $file['tmp_name'];
+            $fileError= $file['error'];
+            $fileSize = $file['size'];
+            $fileExt = explode('.', $fileName);
+        
+         
+            $fileActualExt = strtolower(end($fileExt));
+            //print_r($fileName);
+        
+            $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+         
+            if (in_array($fileActualExt, $allowed)) {
+                if ($fileError ===0) {
+                    if ($fileSize < 8000000) {
+                        $fileNameNew = "profile".$sessionid.".". $fileActualExt;
+                        $fileDestination = 'uploads/'.$fileNameNew;
+        
+                        move_uploaded_file($fileTmpName, $fileDestination);
+                        $stmt = $pdo -> prepare("UPDATE profileimg SET status=1 WHERE userid = ?");
+                        $stmt -> execute([$sessionid]);
+                        $stmt = $pdo -> prepare("UPDATE profileimg SET ext = ? WHERE userid = ?");
+                        $stmt -> execute([$fileActualExt, $sessionid]);
+
+                       
+
+        
+                    }else {
+                        echo 'your file is too big';
+                    }
+                } else{
+                    echo 'There was an error uploading';
+                }
+            }else {
+                echo 'You cannot upload files of this type';
+            }
+            
+        }
+
+
+
 
 
   if(isset($_POST['login-submit'])) {
@@ -40,17 +95,357 @@
     
     ?>
 
+
+
+
+
+
 <?php  
 
-if(isset($_SESSION['userId'])) {
+if(isset($_SESSION['userId'] )) {
+    
+    if(isset($_GET['user'])) {
+        if($_SESSION['userId'] == $_GET['user']) {
+         $getUsersId = $_GET['user'];
+         $stmt = $pdo->prepare("SELECT * FROM posts WHERE inputid = ? ");
+         $stmt->execute([$getUsersId]);
+         $posts = $stmt->fetchAll();
+            
+        echo "<div class='postsContainer'>";
 
-  echo ' <p class ="login-status">You are logged in &#x1F603;	</p>';
-  echo "<br> <h3 class ='login-status'> Welcome " .$_SESSION['username'] ." !</h3>" ;
- //header('Location: posts.php');
-}else{
+
+ foreach($posts as $post) { 
+
+    echo "<div class='containerDivs'>";
+    
+    $stmt = $pdo -> prepare("SELECT * FROM profileimg WHERE userid = ? ");
+    $stmt -> execute([$post->inputId]);
+    $profimg = $stmt->fetch();
+
+    
+
+  
+        
+     if($profimg) {
+   
+        if($profimg->status == 0) {
+            echo "<img class='imgProf' src='uploads/profileDefault.png' height='50' width='50'>" ;
+        }
+    
+        else if($profimg->status == 1){
+            //"<img src='uploads/profile".$profimg->userid.".".$profimg->ext." ' height='50' width='50'>";
+            echo "<img class='imgProf' src='uploads/profile".$profimg->userid.'.'.$profimg->ext."' height='50' width='50'>" ;
+
+        }
+    
+      }
+      
+     else{
+        echo "<img class='imgProf' src='uploads/profileDefault.png' height='50' width='50'>" ;
+    }
+    
+        echo "<form method='post' action='index.php?user=".$profimg->userid."' >
+                <input type='submit' value='see profile'>
+                </form>
+        
+        ";
+        
+
+
+echo "<h3> ".$post->author."</h3>";
+
+ if($_SESSION['userId'] == $post->inputId) { 
+        echo "<div class='btnsWrapper'>
+            <button  class='editBtn'>Edit</button>
+        </div>";
+
+ }  
+      
+
+
+
+
+
+
+
+       
+    if($post->video  && $post->image ) {
+      echo   "<P>$post->body</p>
+      <img class='postImg' src='uploads/".$post->image."' >
+      <video width='300' height='210' controls='controls'>
+      <source src='uploadVideos/".$post->video."' type='video/mp4'>
+      </video>
+      ";  
+    
+  }
+    elseif($post->video ) {
+        echo   "<P>$post->body</p>
+        <video width='300' height='210' controls='controls'>
+        <source src='uploadVideos/".$post->video."' type='video/mp4'>
+        </video>";  
+      
+    }
+    elseif($post->image ) {
+      echo   "<P>$post->body</p>
+      <img class='postImg' src='uploads/".$post->image."'>
+      
+      ";  
+    
+  }else{
+    echo   "<P>$post->body</p>";
+
+}
+    
+  
+  
+echo "</div>";
+
+}  
+
+
+
+echo "</div>";
+
+
+           
+        
+ 
+
+} elseif($_SESSION['userId'] !== $_GET['user']){
+            
+    
+         $getUsersId = $_GET['user'];
+         $stmt = $pdo->prepare("SELECT * FROM posts WHERE inputid = ? ");
+         $stmt->execute([$getUsersId]);
+         $posts = $stmt->fetchAll();
+            
+
+         echo "<div class='postsContainer'>";
+
+
+ foreach($posts as $post) { 
+
+    echo "<div class='containerDivs'>";
+    
+    $stmt = $pdo -> prepare("SELECT * FROM profileimg WHERE userid = ? ");
+    $stmt -> execute([$post->inputId]);
+    $profimg = $stmt->fetch();
+
+    
+
+  
+        
+     if($profimg) {
+   
+        if($profimg->status == 0) {
+            echo "<img class='imgProf' src='uploads/profileDefault.png' height='50' width='50'>" ;
+        }
+    
+        else if($profimg->status == 1){
+            //"<img src='uploads/profile".$profimg->userid.".".$profimg->ext." ' height='50' width='50'>";
+            echo "<img class='imgProf' src='uploads/profile".$profimg->userid.'.'.$profimg->ext."' height='50' width='50'>" ;
+
+        }
+    
+      }
+      
+     else{
+        echo "<img class='imgProf' src='uploads/profileDefault.png' height='50' width='50'>" ;
+    }
+    
+        echo "<form method='post' action='index.php?user=".$profimg->userid."' >
+                <input type='submit' value='see profile'>
+                </form>
+        
+        ";
+        
+
+
+echo "<h3> ".$post->author."</h3>";
+
+ if($_SESSION['userId'] == $post->inputId) { 
+        echo "<div class='btnsWrapper'>
+            <button  class='editBtn'>Edit</button>
+        </div>";
+
+ }  
+      
+
+
+
+
+
+
+
+       
+    if($post->video  && $post->image ) {
+      echo   "<P>$post->body</p>
+      <img class='postImg' src='uploads/".$post->image."' >
+      <video width='300' height='210' controls='controls'>
+      <source src='uploadVideos/".$post->video."' type='video/mp4'>
+      </video>
+      ";  
+    
+  }
+    elseif($post->video ) {
+        echo   "<P>$post->body</p>
+        <video width='300' height='210' controls='controls'>
+        <source src='uploadVideos/".$post->video."' type='video/mp4'>
+        </video>";  
+      
+    }
+    elseif($post->image ) {
+      echo   "<P>$post->body</p>
+      <img class='postImg' src='uploads/".$post->image."'>
+      
+      ";  
+    
+  }else{
+    echo   "<P>$post->body</p>";
+
+}
+    
+  
+  
+echo "</div>";
+
+}  
+
+
+
+echo "</div>";
+
+}
+   
+} 
+
+
+                    // index.php with no endpoints so just your profile page from navbar selection
+else{
 
  
+    $sessionId = $_SESSION['userId'];
+    $stmt = $pdo->prepare("SELECT * FROM posts WHERE inputid = ? ");
+    $stmt->execute([$sessionId]);
+    $posts = $stmt->fetchAll();
+
+
+    echo "<div class='postsContainer'>";
+
+
+ foreach($posts as $post) { 
+
+    echo "<div class='containerDivs'>";
+    
+    $stmt = $pdo -> prepare("SELECT * FROM profileimg WHERE userid = ? ");
+    $stmt -> execute([$post->inputId]);
+    $profimg = $stmt->fetch();
+
+    
+
+  
+        
+     if($profimg) {
+   
+        if($profimg->status == 0) {
+            echo "<img class='imgProf'  src='uploads/profileDefault.png' height='50' width='50'>" ;
+        }
+    
+        else if($profimg->status == 1){
+            //"<img src='uploads/profile".$profimg->userid.".".$profimg->ext." ' height='50' width='50'>";
+            echo "<img class='imgProf'  src='uploads/profile".$profimg->userid.'.'.$profimg->ext."' height='50' width='50'>" ;
+
+        }
+    
+      }
+      
+     else{
+        echo "<img class='imgProf'  src='uploads/profileDefault.png' height='50' width='50'>" ;
+    }
+    
+        echo "<form method='post' action='index.php?user=".$profimg->userid."' >
+                <input type='submit' value='see profile'>
+                </form>
+        
+        ";
+        
+
+
+echo "<h3> ".$post->author."</h3>";
+
+ if($_SESSION['userId'] == $post->inputId) { 
+        echo "<div class='btnsWrapper'>
+            <button  class='editBtn'>Edit</button>
+        </div>";
+
+ }  
+      
+
+
+
+
+
+
+
+       
+    if($post->video  && $post->image ) {
+      echo   "<P>$post->body</p>
+      <img class='postImg' src='uploads/".$post->image."' >
+      <video width='300' height='210' controls='controls'>
+      <source src='uploadVideos/".$post->video."' type='video/mp4'>
+      </video>
+      ";  
+    
+  }
+    elseif($post->video ) {
+        echo   "<P>$post->body</p>
+        <video width='300' height='210' controls='controls'>
+        <source src='uploadVideos/".$post->video."' type='video/mp4'>
+        </video>";  
+      
+    }
+    elseif($post->image ) {
+      echo   "<P>$post->body</p>
+      <img class='postImg' src='uploads/".$post->image."'>
+      
+      ";  
+    
+  }else{
+    echo   "<P>$post->body</p>";
+
 }
+    
+  if($_SESSION['userId'] == $post->inputId) {
+        
+    echo "<div class='editAbsoluteDiv'>
+        <form class='formToEdit' action='posts.php' method='POST'>
+            <ion-icon name='close-outline'></ion-icon>
+            <textarea name='the_body' type='text' value='". $post->body."' >echo $post->body </textarea>
+            
+            <input name='the_id' type='hidden' value='".$post->id."'>
+            <input name='submit' type='submit' value='Save Changes'>
+            <button  name='delete' type='submit' class='deleteBtn'>Delete</button>
+        </form>
+    </div>";
+        
+        
+        
+        
+     }     
+  
+echo "</div>";
+
+}  
+
+
+
+echo "</div>";
+
+}
+   
+ 
+
+} 
 
 
 ?>
