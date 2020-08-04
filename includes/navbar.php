66417b9a842f57ce0dfc46051d858501959f2919
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-08-04 09:32:44 -0500

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,19 +1,58 @@
 
 <?php 
-    echo '<link rel="stylesheet" href="../style.css">'
+   // echo '<link rel="stylesheet" href="../style.css">'
 ?>
 <div class="containAll">
 
 <nav class="navbar">
     <ul>
-        <a href="index.php"><li><ion-icon name="home-outline"></ion-icon> Home</li></a>
-        <a href="posts.php"><li> &#x1F4C4; Newsfeed</li></a>
-        <a href="post.php"><li> &#x1F4E5; Post</li></a>
+    <?php  if(isset($_SESSION['userId'])) {
+         $sessionid =  $_SESSION['userId'];
+         //   echo "Your session ID =  $sessionid";
+           $stmt = $pdo -> prepare("SELECT * FROM profileimg WHERE userid = ? ");
+           $stmt -> execute([$sessionid]);
+           $profimg = $stmt->fetch();
+          
+         
+           if($profimg) {
+            
+             if($profimg->status == 0) {
+                 echo "<img class='imgProf' id='imgProf' 
+                  src='uploads/profileDefault.png'  height='60' width='60' ".mt_rand().">   ";
+                }
+         
+             if($profimg->status == 1){
+                 echo "<img class='imgProf' id='imgProf'  src='uploads/profile".$profimg->userid.".".$profimg->ext." ' height='60' width='60' ".mt_rand().">";   
+                }
+         
+           }
+           
+          else{
+             echo "<img class='imgProf' id='imgProf'  src='uploads/profileDefault.png'  height='60' width='60'  ".mt_rand().">   ";
+            }
+     
+        }     
+
+
+
+
+        ?>
+            
+            <li><form class='profileUploadForm' id='profileUploadForm' method='post' action='index.php' enctype='multipart/form-data'  >
+        <input type='file' name='profileUpload' id='profileUpload' hidden='hidden' >
+        <label for='profileUpload' class='profileUploadLabel' id='profileUploadLabel'  >Profile Picture</label>
+        <input type='submit' name='submit_new_pro_pic' id='submit_new_pro_pic' value='Upload Selected Profile Picture'   >
+    
+        </form></li>
+            
+        <a href="index.php"><li> Profile</li></a>
+        <a href="posts.php"><li> Newsfeed</li></a>
+        <a href="post.php"><li> Post</li></a>
 
     <?php  if(isset($_SESSION['userId'])){ ?>
         
         <li><form class="logout" action="includes/logout.php" method="post">
-        <button type="submit" name="logout-submit">&#x274C; Logout</button>
+        <button type="submit" name="logout-submit">Logout</button>
             </form></li>
 
     <?php } ?>        
