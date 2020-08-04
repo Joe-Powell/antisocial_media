8f519f20351a6afa7a7a0ef276f82159a26d331f
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-08-04 09:31:40 -0500

    Update addpostform.php

--- addpostform.php
+++ addpostform.php
@@ -3,6 +3,13 @@
     <form class="add-post" method="POST" action="post.php" enctype='multipart/form-data' >
          <input type="hidden" name="author" value="<?php echo  $_SESSION['username'] ?>">
          <input type="hidden" name="inputId" value="<?php echo $_SESSION['userId']; ?>">
+         
+
+
+
+
+
+
 
        <div class='textAreaDiv'>
             <h2>Add a new post here</h2>
