2f834f84017d23b781463ceb10c6385ad5ba7c6a
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-08-10 22:46:15 -0500

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,7 @@
 
-
+<?php 
+   // echo '<link rel="stylesheet" href="../style.css">'
+?>
 <div class="containAll">
 
 <nav class="navbar">
@@ -20,7 +22,7 @@
                 }
          
              if($profimg->status == 1){
-                 echo "<img class='imgProf' id='imgProf'  src='uploads/profile".$profimg->userid.".".$profimg->ext." ' height='60' width='60' ".mt_rand().">";   
+                 echo "<img class='imgProf' id='imgProf'  src='uploads/profile".$profimg->userid.".".$profimg->ext." ' height='60' width='60' ".mt_rand()."> ";   
                 }
          
            }
@@ -36,12 +38,13 @@
 
         ?>
             
-            <li><form class='profileUploadForm' id='profileUploadForm' method='post' action='index.php' enctype='multipart/form-data'  >
-        <input type='file' name='profileUpload' id='profileUpload' hidden='hidden' >
-        <label for='profileUpload' class='profileUploadLabel' id='profileUploadLabel'  >Profile Picture</label>
-        <input type='submit' name='submit_new_pro_pic' id='submit_new_pro_pic' value='Upload Selected Profile Picture'   >
     
-        </form></li>
+        <form class='profileUploadForm' id='profileUploadForm' method='post' action='index.php' enctype='multipart/form-data'  >
+            <input type='file' name='profileUpload' id='profileUpload' hidden='hidden' >
+            <label for='profileUpload' class='profileUploadLabel' id='profileUploadLabel'  >Select Profile Picture</label><br>
+            <input type='submit' name='submit_new_pro_pic' id='submit_new_pro_pic' value='Upload Profile Picture'   >
+        </form>
+    
             
         <a href="index.php"><li> Profile</li></a>
         <a href="posts.php"><li> Newsfeed</li></a>
