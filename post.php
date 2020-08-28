90b271c0f728e409999a0d29e9e62f302ba0d190
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-08-28 12:01:53 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,6 +1,10 @@
-<?php session_start(); ?>
+<?php session_start(); 
+require "./config/db.php";
+require "./includes/header.php";
 
-<?php require "./includes/header.php";?>
+
+
+?>
 
 <?php
 
@@ -8,44 +12,60 @@
 if(isset($_POST['submit'])) {
     require "./config/db.php";
 
-    $file = $_FILES["file"];
-    //print_r($file);
-    $fileName = $file['name'];
-    $fileType = $file['type'];
-    $fileTmpName = $file['tmp_name'];
-    $fileError= $file['error'];
-    $fileSize = $file['size'];
-    $fileExt = explode('.', $fileName);
-    $fileActualExt = strtolower(end($fileExt));
-    //print_r($fileTmpName);
-    $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+    if(isset($_FILES["file"])) {
+        $file = $_FILES["file"];
+        //print_r($file);
+        $fileName = $file['name'];
+        $fileType = $file['type'];
+        $fileTmpName = $file['tmp_name'];
+        $fileError= $file['error'];
+      
+        print_r($fileTmpName);
+      
 
-    $explode_filename = explode('.', $fileName);
-    $newFileName = $explode_filename[0];
+        $fileDestination = 'uploads/'.$fileName;
 
-    $fileNameNew = $newFileName . "." . $fileActualExt;   //  the file and extension, I already made image column in database to catch it
+      
 
-    $fileDestination = 'uploads/'.$fileNameNew;
+        move_uploaded_file($fileTmpName, $fileDestination);
 
-    if (in_array($fileActualExt, $allowed)) {
-        if ($fileSize < 1000000) {
-            move_uploaded_file($fileTmpName, $fileDestination);
+        
 
-        }
-
+        $body = $_POST['body'];
+        $author = $_POST['author'];
+        $inputId = $_POST['inputId'];
+        
 
 
+
+
+
+
+        if(isset($_FILES["videoFile"])) {
+            $files = $_FILES["videoFile"];
+            $fileNames = $files['name'];
+            $fileErrors = $files['error'];
+            $fileTypes = $files['type'];
+            $fileTmpNames = $files['tmp_name'];
+          
+            $fileSizes = $files['size'];
+                
+            
+            $fileDestinations = 'uploadVideos/'.$fileNames;
+                move_uploaded_file($fileTmpNames, $fileDestinations);
+    
+            
+        }   
+    
+
+      
     }
 
-    $body = $_POST['body'];
-    $author = $_POST['author'];
-    $inputId = $_POST['inputId'];
-    $fileNameNew = $newFileName . "." . $fileActualExt;  // just declairing again to show whats happening
-
-     $stmt = $pdo->prepare("INSERT INTO posts (author, body, inputId, image ) Values(?, ?, ?, ?)");
-     $stmt->execute([ $author, $body, $inputId, $fileNameNew ]);
+    $stmt = $pdo->prepare("INSERT INTO posts (author, body, inputId, image, video ) Values(?, ?, ?, ?, ?)");
+    $stmt->execute([ $author, $body, $inputId, $fileName, $fileNames ]);
     
-     //header('Location: posts.php');
+
+
 
 
 }
@@ -58,7 +78,6 @@ if(isset($_SESSION['userId'])) {
     
     
 }else{
-    //header('Location: http://myblogs.live/loginSocialMediaPosts/index.php');
     
     echo 'session not started here';
 }
