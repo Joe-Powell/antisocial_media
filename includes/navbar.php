df73570128dbc0f22748f589b448efbe10d8c8bf
Joe Powell <joe_773@yahoo.com>
2020-11-22 21:25:24 -0600

    navigation bar improvements

--- includes/navbar.php
+++ includes/navbar.php
@@ -49,15 +49,18 @@
 
 <ul>  
    
-        <a href="index.php"><li>Profile</li></a>
-        <a href="posts.php"><li>Feed</li></a>
-        <a href="post.php"><li>Post</li></a>
+        <a href="index.php"><li>Home</li>
+        <i class="fas fa-home"></i></a>
+        <a href="posts.php"><li>Feed</li>
+        <i class="fas fa-user-friends"></i></a>
+        <a href="post.php"><li>Post</li>
+        <i class="fas fa-download"></i></a>
 
     <?php  if(isset($_SESSION['userId'])){ ?>
         
         <li><form class="logout" action="includes/logout.php" method="post">
         <button type="submit" name="logout-submit">Logout</button>
-            </form></li>
+            </form><i class="fab fa-expeditedssl"></i></li>
 
     <?php } ?>        
 
