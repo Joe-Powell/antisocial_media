36e930e078556b549c12af0095bb2b64864fe211
Joe Powell <joe_773@yahoo.com>
2021-04-14 10:35:12 -0500

    changes

--- config/db.php
+++ config/db.php
@@ -1,16 +1,16 @@
 <?php
- $host = 'localhost';
- $user = 'root';
- $password = '12345';
- $dbname = 'databaseboth';
+//  $host = 'localhost';
+//  $user = 'root';
+//  $password = '12345';
+//  $dbname = 'databaseboth';
  
- $dsn = 'mysql:host=' . $host . '; dbname=' . $dbname;
+//  $dsn = 'mysql:host=' . $host . '; dbname=' . $dbname;
  
- try {
- $pdo = new PDO($dsn, $user, $password);
- $pdo->setattribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
- $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "Connected successfully";
- } catch(PDOException $e){
-     echo "Connection failed: " . $e->getMessage();
- }
+//  try {
+//  $pdo = new PDO($dsn, $user, $password);
+//  $pdo->setattribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+//  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   //echo "Connected successfully";
+//  } catch(PDOException $e){
+//      echo "Connection failed: " . $e->getMessage();
+//  }
